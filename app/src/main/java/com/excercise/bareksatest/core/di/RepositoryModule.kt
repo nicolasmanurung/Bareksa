2524bcddb07183eb45a79113773d21950f65f5dc
nick2905 <nicolasmanurung16@gmail.com>
2021-08-26 17:32:06 +0700

    di module setup

---
+++ app/src/main/java/com/excercise/bareksatest/core/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package com.excercise.bareksatest.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module(includes = [NetworkModule::class, DatabaseModule::class])
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+}
